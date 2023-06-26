f1a61e8008fd6288c5d2ff8e23cc49e208c47506
superjd <92049184+super-jd@users.noreply.github.com>
2023-06-26 17:33:31 -0400

    clean to restore 2

--- libs/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$BDD_host="localhost";
-$BDD_user="root";
-$BDD_password="password"; // vide sous windows
-$BDD_base="gigachad"; // nom de la base de données
-
-?>
--- libs/modele.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-include_once("maLibSQL.pdo.php");
-
-
-?>
