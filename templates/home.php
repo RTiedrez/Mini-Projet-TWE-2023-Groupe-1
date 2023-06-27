1c54bbe58592c6dfcd54a4a0168508f230ec66ba
LucasSLD <lucassld700@gmail.com>
2023-06-27 09:40:56 +0200

    Feat : moved home style in css/style.css

--- templates/home.php
+++ templates/home.php
@@ -1,66 +1,12 @@
-<!-- A déplacer plus tard dans une feuille de style et dans le head de header.php ? -->
-<head>
-<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-<link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
-
-	<style type="text/css">
-		
-		.rounded-box{
-			border-radius: 17px;
-		}
-		
-		#corps {
-			padding-left: 200px;
-			padding-right: 200px;
-			padding-top: 40px;
-			padding-bottom: 30px;
-			max-width: 1000px;
-			width: fit-content;
-			margin-left: auto;
-			margin-right: auto;
-			font-family: 'Bebas Neue';
-		}
-
-		.black-transparent-background{
-			background-color: rgba(0,0,0,.8);
-		}
-
-		.red{
-			color: red;
-		}
-
-		h1 {
-			color: white;
-			font-size: 100px;
-		}
-
-		#corps p {
-			color: white;
-			font-size: 50px;
-		}
-
-		.material-symbols-outlined {  
-			font-size: 50px;
-			font-variation-settings:
-				'FILL' 0,
-				'wght' 500,
-				'GRAD' 0,
-				'opsz' 48
-		}
-	</style>
-</head>
-
-
 <?php
 if (basename($_SERVER["PHP_SELF"]) != "index.php")
 {
 	header("Location:../index.php?view=home");
 	die("");
 }
-
 ?>
 
-<div id="corps" class="black-transparent-background rounded-box">
+<div id="homeDiv" class="black-transparent-background rounded-box">
 	<h1><span class="red">READY</span> </br> TO LIFT ?</h1>
 	<p>
 		<span class="material-symbols-outlined" style="color: red;">
